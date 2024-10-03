e7de128512881ba2f0bbb106888d1c7e8382dd9c
  <>
2024-10-03 12:00:00 +0300

    configurea vähän parannettu

--- configure.sh
+++ configure.sh
@@ -25,11 +25,14 @@ make_dependency() {
     printf "use_$name = 1\n" >> $file
 }
 
-printf "# Automatically created by $0. Comment out those which you don't want.\n" > $file
+printf "# Automatically created by $0. Comment out those which you don't want.\n\n" > $file
 
-make_dependency libttra ttra
-make_dependency libwaylandhelper waylandhelper
+printf "# Needed by cplot_write_png\n" >> $file
+make_dependency libpng png
 printf "# Needed by cplot_write_mp4\n" >> $file
 make_dependency ffmpeg avcodec avutil avformat
+printf "\n# Always used but whether to link dynamically or download and link statically.\n" >> $file
+make_dependency libttra ttra
+make_dependency libwaylandhelper waylandhelper
 
 rm -f $tmpfile
