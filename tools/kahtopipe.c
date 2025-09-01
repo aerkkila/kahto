6752ffc076321abbb7b41a50a95c730d6f69c6e7
Anttoni Erkkilä <anttoni.erkkila@protonmail.com>
2025-09-01 08:00:00 +0000

    yksinkertaisempi kahtopipe

--- tools/kahtopipe.c
+++ tools/kahtopipe.c
@@ -12,19 +12,15 @@ int main(int argc, char **argv) {
 	char *format = separator ? "%lf %*c" : "%lf ";
 	long len = 2000;
 	double *data = malloc(len*sizeof(double));
-	double *ptr = data - 1;
-	double *end = data + len;
+	long pos = -1;
 	while (1) {
-		while (++ptr < end)
-			if (scanf(format, ptr) != 1)
+		while (++pos < len)
+			if (scanf(format, data+pos) != 1)
 				goto done;
 		len *= 1.5;
-		long pos = ptr - data;
 		data = realloc(data, len*sizeof(double));
-		ptr = data + pos;
-		end = data + len;
 	}
 done:
-	kahto_show(kahto_y(data, ptr-data));
+	kahto_show(kahto_y(data, pos+1));
 	free(data);
 }
