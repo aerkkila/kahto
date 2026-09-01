8c734a629ac1e55db6aa581f3567992f167da1b8
Anttoni Erkkilä <anttoni.erkkila@protonmail.com>
2026-09-01 08:00:00 +0000

    nclangcomplete lisätty

---
+++ .nclangcomplete.lua
@@ -0,0 +1,7 @@
+-- This is a configuration file for nclangcomplete autocompletion plugin for neovim.
+-- https://codeberg.org/aerkkila/nclangcomplete
+
+return {
+	['kahto_ffmpeg.c'] = {nil, nil},
+	default = {{'-DHAVE_PNG'}, 'kahto.c'},
+};
